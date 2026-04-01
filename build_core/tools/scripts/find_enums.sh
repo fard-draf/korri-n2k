5d2b270e7ebd9731ddb6fb0892131ff2dcff5597
fard-draf <fard@ik.me>
2026-04-01 11:57:22 +0200

    remove build_core/tools

--- build_core/tools/scripts/find_enums.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Utility script: open the generated lookup file for inspection.
-
-cd "$DEV_PATH/warehouse/projects/professional/korrigan/libs/korri-n2k/" || return
-find ../../target -name "generated_lookups.rs" -exec hx {} \;
--- build_core/tools/scripts/find_pgns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Open the generated PGN Rust code for a quick inspection.
-
-cd "$DEV_PATH/warehouse/projects/professional/korrigan/libs/korri-n2k/" || return
-find ../../target -name "generated_pgns.rs" -exec hx {} \;
