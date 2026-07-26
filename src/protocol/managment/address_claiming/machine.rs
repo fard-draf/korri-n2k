085637ed590cca94ec862a1f582065c9259fb199
fard-draf <fard@ik.me>
2026-07-26 14:24:59 +0200

    feat(addr_claiming): give AddressClaimer an Idle and a Listening state

--- src/protocol/managment/address_claiming/machine.rs
+++ src/protocol/managment/address_claiming/machine.rs
@@ -2,7 +2,8 @@ use crate::{
     error::ClaimFault,
     protocol::{
         managment::address_claiming::{
-            is_addr_capable_and_isoname_match, AddressClaimIterator, AddressClaimStrategy,
+            build_address_claim_frame, is_addr_capable_and_isoname_match, AddressClaimIterator,
+            AddressClaimStrategy,
         },
         transport::can_frame::CanFrame,
     },
@@ -14,31 +15,82 @@ pub enum ClaimAction {
     Done(u8),
 }
 
-pub struct AddressClaimer<'a> {
-    my_name: u64,
-    addr_to_claim: Option<u8>,
-    addr_iterator: AddressClaimIterator<'a>,
-    deadline_ms: u64,
+#[derive(PartialEq, Eq)]
+enum State {
+    Idle,
+    Listening { frame: CanFrame, deadline_ms: u64 },
 }
 
+pub struct AddressClaimer<'a> {
+    my_name: Option<u64>,
+    addr_iterator: Option<AddressClaimIterator<'a>>,
+    state: State,
+    is_first: bool,
+}
+#[allow(dead_code)]
 impl<'a> AddressClaimer<'a> {
+    pub fn new(frame: CanFrame) -> Self {
+        AddressClaimer {
+            my_name: None,
+            state: State::Listening {
+                frame,
+                deadline_ms: 250,
+            },
+            addr_iterator: None,
+            is_first: true,
+        }
+    }
+
     pub fn poll(
         &mut self,
         now_ms: u64,
         rx: Option<&CanFrame>,
         strategy: AddressClaimStrategy,
     ) -> Result<ClaimAction, ClaimFault> {
-        if !is_addr_capable_and_isoname_match(self.my_name, strategy) {
-            return Err(ClaimFault::InconsistentStrategy);
-        }
-        if let None = rx {
-            return Ok(ClaimAction::Wait(Some(250)));
-        }
-        // Conflit
-        if let Some(rx) = rx {
-            Ok(ClaimAction::Send(rx.clone()))
+        if let Some(my_name) = self.my_name {
+            // not started | first call
+            // guard
+            if !is_addr_capable_and_isoname_match(my_name, strategy) {
+                return Err(ClaimFault::InconsistentStrategy);
+            }
+            match self.state {
+                State::Idle => ,
+                State::Listening { frame, deadline_ms } => todo!(),
+            }
+            if self.state == State::Listening && self.is_first {
+                if let Some(addr_to_claim) = self.state {
+                    self.is_first_call = false;
+                    let claim_frame = build_address_claim_frame(my_name, addr_to_claim)
+                        .map_err(ClaimFault::BuildErr)?;
+                    return Ok(ClaimAction::Send(claim_frame));
+                }
+            }
+            // listen     | rx == None && now <  deadline
+            if !self.is_first_call && !self.is_ended && now_ms < self.deadline_ms && rx.is_none() {
+                self.deadline_ms -= now_ms;
+                return Ok(ClaimAction::Wait(Some(self.deadline_ms)));
+            }
+            // listen     | rx == None && now >= deadline
+            if !self.is_first_call && !self.is_ended && now_ms >= self.deadline_ms && rx.is_none() {
+                self.deadline_ms -= now_ms;
+            }
+
+            // listen     | frame without conflict
+            // listen     | conflict >> my_name wins
+            // listen     | conflict >> my_name loses
+            // listen     | conflict >> lose, no more address available
+            if let None = rx {
+                return Ok(ClaimAction::Wait(Some(250)));
+            }
+            // Conflit
+            if let Some(rx) = rx {
+                Ok(ClaimAction::Send(rx.clone()))
+            } else {
+                Ok(ClaimAction::Wait(Some(self.deadline_ms - now_ms)))
+            }
         } else {
-            Ok(ClaimAction::Wait(Some(self.deadline_ms - now_ms)))
+            // TODO: create a better err
+            Err(ClaimFault::InconsistentStrategy)
         }
     }
 }
